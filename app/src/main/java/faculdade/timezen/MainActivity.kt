4f408e363c1362a55b48d121eab7400e82350f12
matkijahenkilo <86783808+matkijahenkilo@users.noreply.github.com>
2023-04-23 23:57:04 -0300

    Create pomodoro loop sketch

--- app/src/main/java/faculdade/timezen/MainActivity.kt
+++ app/src/main/java/faculdade/timezen/MainActivity.kt
@@ -2,10 +2,29 @@ package faculdade.timezen
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import faculdade.timezen.Plan.Plan
+import faculdade.timezen.Plan.Timer
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    fun startTimeCounter(view: View) {
+        val countTimeTextView: TextView = findViewById(R.id.countTime)
+        val planNameTextView: TextView = findViewById(R.id.planName)
+        val planStageTextView: TextView = findViewById(R.id.planStage)
+
+        val timer = Timer(countTimeTextView, 3000, 2000, 3)
+        val plan = Plan("lololol", timer);
+
+        plan.startPlanLoop()
+    }
+
+    fun stopTimeCounter(view: View) {
+        //TODO
     }
 }
