f530ae7d39569739682422d57d0eeb972dbe32b5
Fernando Adaltino <adaltino.dev@gmail.com>
2023-05-17 08:58:04 -0300

    colocando o código do pomodoro na activity certa

--- app/src/main/java/faculdade/timezen/ui/activity/MainActivity.kt
+++ app/src/main/java/faculdade/timezen/ui/activity/MainActivity.kt
@@ -19,49 +19,8 @@ import java.util.Timer
 import java.util.prefs.Preferences
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        // https://developer.android.com/topic/libraries/architecture/datastore
-        val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "prefs")
-    }
-
-    private lateinit var pomodoro: Pomodoro
-    private val timer = Timer()
-    private val pomodoroTimer = PomodoroTimer(5000, 3000, 3)
-    private val plan = Plan("alonso", pomodoroTimer)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        initializeFunctions()
-    }
-
-    private fun initializeFunctions() {
-        val counterTextView: TextView = findViewById(R.id.countTime)
-        val planNameTextView: TextView = findViewById(R.id.planName)
-        val planStageTextView: TextView = findViewById(R.id.planStage)
-        val pomodoroTextViews =
-            PomodoroTextViews(counterTextView, planNameTextView, planStageTextView)
-        pomodoro =
-            Pomodoro(plan, InfoManipulator(pomodoroTextViews, dataStore))
-
-
-        findViewById<Button>(R.id.start_button)
-            .setOnClickListener {
-                Log.d("Button", "start pomodoro button clicked")
-                Snackbar.make(it, "pomodoro iniciado", 2000).show()
-                pomodoro.start()
-            }
-
-        findViewById<Button>(R.id.reset_button)
-            .setOnClickListener {
-                //TODO()
-            }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        timer.cancel()
     }
 }
--- app/src/main/java/faculdade/timezen/ui/activity/SelectedPlanActivity.kt
+++ app/src/main/java/faculdade/timezen/ui/activity/SelectedPlanActivity.kt
@@ -1,12 +1,67 @@
 package faculdade.timezen.ui.activity
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.preferencesDataStore
+import com.google.android.material.snackbar.Snackbar
 import faculdade.timezen.R
+import faculdade.timezen.planModel.Plan
+import faculdade.timezen.planModel.Pomodoro
+import faculdade.timezen.planModel.PomodoroTimer
+import faculdade.timezen.ui.activity.SelectedPlanActivity.Companion.dataStore
+import faculdade.timezen.utils.InfoManipulator
+import faculdade.timezen.utils.PomodoroTextViews
+import java.util.Timer
+import java.util.prefs.Preferences
 
 class SelectedPlanActivity : AppCompatActivity() {
+    companion object {
+        // https://developer.android.com/topic/libraries/architecture/datastore
+        val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "prefs")
+    }
+
+    private lateinit var pomodoro: Pomodoro
+    private val timer = Timer()
+    private val pomodoroTimer = PomodoroTimer(5000, 3000, 3)
+    private val plan = Plan("alonso", pomodoroTimer)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selected_plan)
+
+        initializeFunctions()
+    }
+
+    private fun initializeFunctions() {
+        val counterTextView: TextView = findViewById(R.id.countTime)
+        val planNameTextView: TextView = findViewById(R.id.planName)
+        val planStageTextView: TextView = findViewById(R.id.planStage)
+        val pomodoroTextViews =
+            PomodoroTextViews(counterTextView, planNameTextView, planStageTextView)
+        pomodoro =
+            Pomodoro(plan, InfoManipulator(pomodoroTextViews, dataStore))
+
+
+        findViewById<Button>(R.id.start_button)
+            .setOnClickListener {
+                Log.d("Button", "start pomodoro button clicked")
+                Snackbar.make(it, "pomodoro iniciado", 2000).show()
+                pomodoro.start()
+            }
+
+        findViewById<Button>(R.id.reset_button)
+            .setOnClickListener {
+                //TODO()
+            }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
     }
 }
